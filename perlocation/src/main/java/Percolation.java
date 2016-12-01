e8f7eb15f7fe75f08bd52926e4d9919bf70b488c
Michal Knobel <michal.knobel@gmail.com>
2016-12-01 16:56:42 +0100

    implemented isOpen and open in percolation

--- perlocation/src/main/java/Percolation.java
+++ perlocation/src/main/java/Percolation.java
@@ -1,31 +1,82 @@
 /**
  * Created by Michal Knobel on 01.12.2016.
  */
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
+
+  private boolean array[];
+  private WeightedQuickUnionUF weightedQuickUnionUF;
+  private int size;
+  private int top[];
+  private int bottom[];
+
   public Percolation(int n)                // create n-by-n grid, with all sites blocked
   {
-    if (n <= 0) throw new IllegalArgumentException();
+    if (n <= 0)
+      throw new IllegalArgumentException("Percolation array size shouldn't be lower then 1");
+
+    size = n;
+    array = new boolean[size * size];
+    for (int i = 0; i < size; i++) {
+      array[i] = false;
+    }
+
+//    top = new int[n];
+//    bottom = new int[n];
+
+    weightedQuickUnionUF = new WeightedQuickUnionUF(size);
+  }
+
+  public static void main(String[] args) {
 
   }
-  public void open(int row, int col)       // open site (row, col) if it is not open already
-  {
 
+  public void open(int row, int col) {
+
+    int index = returnIndex(row, col);
+    array[index] = true;
+
+    //up
+    if (row > 1 && isOpen(row - 1, col)) {
+      int indexU = returnIndex(row - 1, col);
+      weightedQuickUnionUF.union(index, indexU);
+    }
+    //left
+    if (col > 1 && isOpen(row, col - 1)) {
+      int indexL = returnIndex(row, col - 1);
+      weightedQuickUnionUF.union(index, indexL);
+    }
+    //right
+    if (col < size && isOpen(row, col + 1)) {
+      int indexR = returnIndex(row, col + 1);
+      weightedQuickUnionUF.union(index, indexR);
+    }
+    //down
+    if (col > size && isOpen(row + 1, col)) {
+      int indexD = returnIndex(row + 1, col);
+      weightedQuickUnionUF.union(index, indexD);
+    }
   }
-  public boolean isOpen(int row, int col)  // is site (row, col) open?
-  {
+
+  public boolean isOpen(int row, int col) {
+    return array[returnIndex(row, col)];
+  }
+
+  public boolean isFull(int row, int col) {  // is site (row, col) full?
+    int index = returnIndex(row, col);
+
     return false;
   }
-  public boolean isFull(int row, int col)  // is site (row, col) full?
-  {
-    return false;
-  }
+
   public boolean percolates()              // does the system percolate?
   {
     return false;
   }
-  public static void main(String[] args)   // test client (optional)
-  {
 
+  private int returnIndex(int row, int col) {
+    return ((row - 1) * size - 1) + col;
   }
 
 }
